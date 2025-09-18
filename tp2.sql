4f78bcd1fef0acb03430f1249036f10367f6cd31
YoanMln <yoan.moulin@hotmail.fr>
2025-09-18 14:57:30 +0200

    requête sql exercice 2

--- tp2.sql
+++ tp2.sql
@@ -3,4 +3,17 @@ USE test_1;
 SELECT * FROM users ORDER BY salary DESC LIMIT 1;
 
 
+-- Exercice 2 -- 
+
+USE test_2;
+SELECT
+    d.name AS department_name,
+    u_manager.name AS manager_name,
+    COUNT(e.user_id) AS employee_count
+FROM department d
+JOIN users u_manager ON d.manager_id = u_manager.id
+LEFT JOIN employees e ON e.user_id = u_manager.id OR e.user_id IN (SELECT id FROM users)
+JOIN users u ON e.user_id = u.id
+GROUP BY d.name, u_manager.name
+ORDER BY d.name;
 
